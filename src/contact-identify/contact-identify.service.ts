2fafe623ac4d17e00cadab74bf626964a76a3fe6
Janarthanan-Balasubramani <janarthananbalasubramani2024@gmail.com>
2024-03-30 22:03:22 +0530

    schema-changes

--- src/contact-identify/contact-identify.service.ts
+++ src/contact-identify/contact-identify.service.ts
@@ -6,65 +6,67 @@ import { PrismaService } from 'src/prisma.service';
 export class ContactIdentifyService {
   constructor(private readonly prismaService: PrismaService) {}
   async create(createContactIdentifyDto: CreateContactIdentifyDto) {
-    const isMobileOrEmailAlreadyExist =
-      await this.prismaService.contact.findFirst({
-        where: {
-          OR: [
-            {
-              email: {
-                equals:createContactIdentifyDto.email
-              },
-            },
-            {
-             phoneNumber:{
-              equals:createContactIdentifyDto.mobile
+//     const isMobileOrEmailAlreadyExist =
+//       await this.prismaService.contact.findFirst({
+//         where: {
+//           OR: [
+//             {
+//               email: {
+//                 equals:createContactIdentifyDto.email
+//               },
+//             },
+//             {
+//              phoneNumber:{
+//               equals:createContactIdentifyDto.mobile
 
-             }
-            },
-          ],
-        },
-        select: {
-          id: true,
-          email: true,
-          phoneNumber: true,
-        },
-      });
-console.log()
-    if (isMobileOrEmailAlreadyExist) {
-      console.log('isMobileOrEmailAlreadyExist');
-      await this.prismaService.contact.create({
-        data: {
-          email: createContactIdentifyDto.email,
-          phoneNumber: createContactIdentifyDto.mobile,
-          linkPrecedence: 'Secondary',
-          linkedId: isMobileOrEmailAlreadyExist.id,
-        },
-      });
-      return {
-        message: 'created successfully',
-      };
-    }
-    await this.prismaService.contact.create({
-      data: {
-        email: createContactIdentifyDto.email,
-        phoneNumber: createContactIdentifyDto.mobile,
-        linkPrecedence: 'Primary',
-      },
-    });
+//              }
+//             },
+//           ],
+//         },
+//         select: {
+//           id: true,
+//           email: true,
+//           phoneNumber: true,
+//         },
+//       });
+// console.log()
+//     if (isMobileOrEmailAlreadyExist) {
+//       console.log('isMobileOrEmailAlreadyExist');
+//       await this.prismaService.contact.create({
+//         data: {
+//           email: createContactIdentifyDto.email,
+//           phoneNumber: createContactIdentifyDto.mobile,
+//           linkPrecedence: 'Secondary',
+//           linkedId: isMobileOrEmailAlreadyExist.id,
+//         },
+//       });
+//       return {
+//         message: 'created successfully',
+//       };
+//     }
+//     await this.prismaService.contact.create({
+//       data: {
+//         email: createContactIdentifyDto.email,
+//         phoneNumber: createContactIdentifyDto.mobile,
+//         linkPrecedence: 'Primary',
+//       },
+//     });
 
 
-    const primaryContactWithSecondaryContactResponse  = await this.prismaService.contact.findMany({
-      where:{
+//     const primaryContactWithSecondaryContactResponse  = await this.prismaService.contact.findMany({
+//       where:{
         
-            linkedId:1
+//             linkedId:1
           
         
-      },
-    })
+//       },
+//     })
   
-    return {
-      message: 'created successfully',
-      primaryContactWithSecondaryContactResponse
-    };
+//     return {
+//       message: 'created successfully',
+//       primaryContactWithSecondaryContactResponse
+//     };
+
+return this.prismaService.contact.findMany({})
   }
 }
